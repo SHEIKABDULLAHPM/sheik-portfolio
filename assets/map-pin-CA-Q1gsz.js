c9dcf805d65a2aa46b6146924ff4e874ee42419d
SHEIKABDULLAHPM <SHEIKABDULLAHPM@users.noreply.github.com>
2025-12-22 20:57:11 +0000

    deploy: 289cbd2e7105674d023f1f240e6820a5391292a9

---
+++ assets/map-pin-CA-Q1gsz.js
@@ -0,0 +1,1 @@
+import{c as a}from"./index-Su-lOsLr.js";const e=a("Mail",[["rect",{width:"20",height:"16",x:"2",y:"4",rx:"2",key:"18n3k1"}],["path",{d:"m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7",key:"1ocrg3"}]]);const i=a("MapPin",[["path",{d:"M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z",key:"2oe9fu"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]]);export{i as M,e as a};
